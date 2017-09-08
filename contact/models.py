aba334eb2cdbf9478f8ef2bc0c5b17ef6fc3a9f2
Aakash Bajaj <aakashbajaj5311@gmail.com>
2017-09-08 13:50:20 +0530

    alter text field

--- contact/models.py
+++ contact/models.py
@@ -13,4 +13,4 @@ class Query(models.Model):
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone_number = models.CharField(max_length=200, validators=[phone_regex], null=False, blank=False)
 	amount = models.CharField(max_length=20, blank=False, null=False)
-	text = models.TextField()
+	text = models.TextField(null=False, blank=False, default='')
