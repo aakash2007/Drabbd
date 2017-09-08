cc27d6017cc453b03d31d86a9957a581bf89217c
Aakash Bajaj <aakashbajaj5311@gmail.com>
2017-09-09 02:09:42 +0530

    working on form

--- contact/views.py
+++ contact/views.py
@@ -2,8 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import *
+from django import forms
 
 # Create your views here.
+class QueryForm(forms.ModelForm):
+
+	class Meta:
+		model = Query
+		fields = ('name', 'email', 'phone_number', 'amount', 'text')
+
 
 def main_page(request):
-	return render(request, 'index.html', {})
+	form = QueryForm()
+	return render(request, 'index.html', {'form': form})
